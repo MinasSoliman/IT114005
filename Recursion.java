c9eb5bd14d3849582c270ee754090783452fa89e
MinasSoliman <70656675+MinasSoliman@users.noreply.github.com>
2020-09-30 20:21:59 -0400

    Add files via upload

---
+++ Recursion.java
@@ -0,0 +1,12 @@
+public class Recursion {
+  public static void main(String[] args) {
+      int num = 0;
+       for(int i =10; i>=0; i--){
+         num += i;
+         }
+         
+         System.out.println(num);
+         
+      }
+        }
+  
